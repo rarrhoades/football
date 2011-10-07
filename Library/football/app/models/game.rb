387a98c5f2d96cebab479728db4056b8c754dd88
Alex Rhoades <rarrhoades@gmail.com>
2011-10-07 16:52:58 -0700

    Fixed broken dependency

--- Library/football/app/models/game.rb
+++ Library/football/app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   belongs_to :home_team, :class_name => "Owner", :foreign_key => "home_team_id"
+  belongs_to :away_team, :class_name => "Owner", :foreign_key => "away_team_id"
 end
