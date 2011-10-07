c113c258e28d48da88eb999702bc4a3e044d317f
Alex Rhoades <rarrhoades@gmail.com>
2011-10-07 14:08:08 -0700

    Added associations for nfl_game class

--- Library/football/app/models/league.rb
+++ Library/football/app/models/league.rb
@@ -1,5 +1,6 @@
 class League < ActiveRecord::Base
   has_many :divisions
+  has_many :drafts
   validates :name, presence: true
   validates :name, uniqueness: true
 
--- Library/football/app/models/team.rb
+++ Library/football/app/models/team.rb
@@ -1,2 +1,10 @@
 class Team < ActiveRecord::Base
+  has_many :players
+  has_many :nfl_games
+
+
+  def full_name
+    "#{location} #{name}"
+  end
+
 end
